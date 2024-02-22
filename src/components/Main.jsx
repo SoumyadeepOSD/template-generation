3580f77c05fc80128e40bef81236d02d0b5fabdc
SoumyadeepOSD <opencontainer255@gmail.com>
2024-02-22 20:46:49 +0530

    text-color changed and copytoclipboard function fixed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const API_KEY = import.meta.env.VITE_SECRET;
+import {toast, Toaster} from "react-hot-toast";
 
 const Main = () => {
     const [sender, setSender] = useState("");
@@ -9,65 +10,42 @@ const Main = () => {
     const [response, setResponse] = useState("");
     const [loading, setLoading] = useState(false);
     const [type, setType] = useState("");
-    const [copystate, setCopyState] = useState(false);
+    const [copied, setCopied] = useState(false);
+    const notify = () => toast.error('Fill all the fields!');
 
     const HandleSubmitAndGenerateResponse = async () => {
         try {
             const genAI = new GoogleGenerativeAI(API_KEY);
             const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-            const promtTemplate = `I want to represent myself as ${sender} and I want to send a very short ${type} type of email to a person whose designation is like ${receiver} concering about ${content}`;
+            if(!sender || !receiver || !content || !type) {
+                notify();
+                return;
+            }
+            const promptTemplate = `I want to represent myself as ${sender} and I want to send a very short ${type} type of email to a person whose designation is like ${receiver} concerning about ${content}`;
             setLoading(true);
-            const result = await model.generateContent(promtTemplate);
-            const response = result.response;
-            const text = response.text();
-            setResponse(text);
+            const result = await model.generateContent(promptTemplate);
+            const response = await result.response.text();
+            setResponse(response);
             setLoading(false);
         } catch (error) {
-            console.log(error.message);
+            console.error('Failed to generate response: ', error);
         }
     }
 
-
-    const copyToClipBoard = () => {
-        const spanElement = document.querySelector('.response');
-        if (!spanElement) {
-            console.error('Span element not found');
-            return;
-        }
-
-        const textToCopy = spanElement.textContent || spanElement.innerText;
-        if (!textToCopy) {
-            console.error('No text to copy');
-            return;
-        }
-
-        navigator.clipboard.writeText(textToCopy)
-            .then(() => {
-                console.log('Text copied to clipboard');
-                setCopyState(true);
-            })
-            .catch((error) => {
-                console.error('Failed to copy text: ', error);
-            });
+    const copyToClipboard = () => {
+        const textArea = document.createElement('textarea');
+        textArea.textContent = response;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+        setCopied(true);
     }
 
     const sendEmail = () => {
-        const spanElement = document.querySelector('.response');
-        if (!spanElement) {
-            console.error('Span element not found');
-            return;
-        }
-    
-        const textToCopy = spanElement.textContent || spanElement.innerText;
-        if (!textToCopy) {
-            console.error('No text to copy');
-            return;
-        }
-    
-        const emailLink = `mailto:?body=${encodeURIComponent(textToCopy)}`;
+        const emailLink = `mailto:?body=${encodeURIComponent(response)}`;
         window.location.href = emailLink;
     }
-    
 
     return (
         <div className={"h-full flex flex-col items-center"}>
@@ -95,27 +73,32 @@ const Main = () => {
                     placeholder="Write complete description of email..."
                     onChange={(e) => setContent(e.target.value)}
                 ></textarea>
-                <h1>Type of your Email?</h1>
+                <h1 className="text-white">Type of your Email?</h1>
                 <div className="flex flex-row">
                     <div className="form-control">
                         <label className="cursor-pointer label">
-                            <span className="label-text">Long & Informal</span>
-                            <input type="checkbox" checked={type === "Long & Informal"? true : false} onClick={()=>setType("Long & Informal")} className="checkbox checkbox-accent" />
+                            <span className="label-text text-white">Long & Informal</span>
+                            <input type="checkbox" checked={type === "Long & Informal"} onClick={()=>setType("Long & Informal")} className="checkbox checkbox-accent" />
                         </label>
                     </div>
                     <div className="form-control">
                         <label className="cursor-pointer label">
-                            <span className="label-text">Concise & Professional</span>
-                            <input type="checkbox" checked={type === "Concise & Professional"? true : false} onClick={()=>setType("Concise & Professional")} className="checkbox checkbox-accent" />
+                            <span className="label-text text-white">Concise & Professional</span>
+                            <input type="checkbox" checked={type === "Concise & Professional"} onClick={()=>setType("Concise & Professional")} className="checkbox checkbox-accent" />
                         </label>
                     </div>
                 </div>
                 <button onClick={HandleSubmitAndGenerateResponse} className="btn btn-outline btn-accent">Generate</button>
+                <Toaster/>
             </div>
             <span className={loading || response ? "response mx-10 lg:mx-10" : ""}>{loading ? <Loading /> : response}</span>
             <section className="flex flex-row gap-10">
-            {response ? <button onClick={copyToClipBoard} className="btn btn-outline btn-accent mt-5">{copystate? "Copied✅" : "Copy to Clipboard"}</button> : ""}
-            {response? <button className="btn btn-outline btn-accent mt-5" onClick={sendEmail}>Send Email Directly</button>:""}
+                {response && !loading &&
+                    <>
+                        <button onClick={copyToClipboard} className="btn btn-outline btn-accent mt-5">{copied ? "Copied ✅" : "Copy to Clipboard"}</button>
+                        <button onClick={sendEmail} className="btn btn-outline btn-accent mt-5">Send Email Directly</button>
+                    </>
+                }
             </section>
         </div>
     );
