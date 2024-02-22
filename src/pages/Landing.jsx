abfdea8fc74d0826b205b4afcb626fe40ba1e5ce
SoumyadeepOSD <opencontainer255@gmail.com>
2024-02-22 18:22:27 +0530

    text-color changed to white

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -11,16 +11,16 @@ const Landing = () => {
       <div className="flex flex-col lg:flex-row justify-between">
         <section className="absolute h-3/6 lg:bottom-40 top-40 text-center lg:text-left mx-16">
           <h1 className="text-3xl lg:text-7xl font-bold loading-text">
-            <p><span className="text-gradient">Enrich</span> your <span className="text-gradient">email</span></p>
-            <p>writing experience</p>
-            <p>with <span className="text-gradient">AI</span></p>
+            <p><span className="text-gradient">Enrich</span > <span className="text-white">your</span> <span className="text-gradient">email</span></p>
+            <p className="text-white">writing experience</p>
+            <p className="text-white">with <span className="text-gradient">AI</span></p>
           </h1>
         </section>
         <section className="absolute right-12 lg:right-20 text-center lg:text-left top-96 lg:top-52 py-12 loading-text h-64 lg:h-2/4 px-5 border-violet-600 border-2 rounded-lg shadow-lg shadow-cyan-500/50">
           <h1 className="text-1xl lg:text-3xl font-semibold">
-            <p>Supercharge your email writing</p>
-            <p>skills with a single-click</p>
-            <p>user-friendly interface</p>
+            <p className="text-white">Supercharge your email writing</p>
+            <p className="text-white">skills with a single-click</p>
+            <p className="text-white">user-friendly interface</p>
           </h1>
         </section>
       </div>
